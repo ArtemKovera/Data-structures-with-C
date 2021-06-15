9cc1fb194efbd55695796e7d2aacbeea9e466c17
Artem Kovera <artemkovera@gmail.com>
2021-06-15 15:38:11 +0300

    interface.h

---
+++ src/interface.h
@@ -0,0 +1,22 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+int linkedListInterface();
+
+int doubleLinkedListInterface();
+
+int circularLinkedListInterface();
+
+int stackInterface();
+
+int queueInterface();
+
+int setInterface();
+
+int chainedHashTableInterface();
+
+int binaryTreeInterface();
+
+int priorityQueueInterface();
+
+#endif
